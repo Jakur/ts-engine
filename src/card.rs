0741ce487978ae9c58f9d37b06a9e9147b2445d9
Justin Kur <Justink97@hotmail.com>
2020-01-27 02:25:05 -0500

    Begin card framework

--- src/card.rs
+++ src/card.rs
@@ -1,5 +1,125 @@
+#![allow(non_camel_case_types)]
+
+use crate::country::Side;
+
 #[derive(Clone, Copy, PartialEq)]
 pub enum Effect {
     ShuttleDiplomacy,
     FormosanResolution,
+}
+
+pub struct Attributes {
+    pub side: Side,
+    pub ops: i8,
+    pub starred: bool,
+    pub scoring: bool,
+}
+
+impl Attributes {
+    fn new(side: Side, ops: i8) -> Attributes {
+        Attributes {
+            side,
+            ops,
+            starred: false,
+            scoring: false,
+        }
+    }
+    fn star(mut self) -> Attributes {
+        self.starred = true;
+        self
+    }
+    fn scoring(mut self) -> Attributes {
+        self.scoring = true;
+        self
+    }
+}
+
+fn init_cards() -> Vec<Attributes> {
+    use Side::*;
+    let c = Attributes::new;
+    vec![
+        c(Neutral, 0), // Dummy
+        c(Neutral, 0).scoring(),
+        c(Neutral, 0).scoring(),
+        c(Neutral, 0).scoring(),
+        c(US, 3),
+        c(US, 3),
+        c(Neutral, 4), // China
+        c(USSR, 3),
+        c(USSR, 2).star(),
+        c(USSR, 2).star(),
+        c(USSR, 1).star(),
+        c(USSR, 2).star(),
+        c(USSR, 1).star(),
+        c(USSR, 2),
+        c(USSR, 3).star(),
+        c(USSR, 1).star(),
+        c(USSR, 3).star(),
+        c(USSR, 3).star(),
+        c(Neutral, 1).star(),
+        c(US, 1).star(),
+        c(Neutral, 2),
+        c(US, 4).star(),
+        c(US, 2).star(),
+        c(US, 4).star(),
+        c(Neutral, 2),
+        c(US, 3).star(),
+        c(US, 1).star(),
+        c(US, 4).star(),
+        c(USSR, 3).star(),
+        c(US, 3),
+        c(USSR, 2), // Decol
+        c(Neutral, 4),
+        c(Neutral, 1),
+        c(USSR, 3).star(),
+        c(Neutral, 4),
+        c(US, 2).star(), // Formosan
+    ]
+}
+
+pub enum Card {
+    Asia_Scoring = 1,
+    Europe_Scoring,
+    Middle_East_Scoring,
+    Duck_and_Cover,
+    Five_Year_Plan,
+    The_China_Card,
+    Socialist_Governments,
+    Fidel,
+    Vietnam_Revolts,
+    Blockade,
+    Korean_War,
+    Romanian_Abdication,
+    Arab_Israeli_War,
+    Comecon,
+    Nasser,
+    Warsaw_Pact_Formed,
+    De_Gaulle_Leads_France,
+    Captured_Nazi_Scientist,
+    Truman_Doctrine,
+    Olympic_Games,
+    NATO,
+    Independent_Reds,
+    Marshall_Plan,
+    Indo_Pakistani_War,
+    Containment,
+    CIA_Created,
+    US_Japan_Mutual_Defense_Pact,
+    Suez_Crisis,
+    East_European_Unrest,
+    Decolonization,
+    Red_Scare_Purge,
+    UN_Intervention,
+    De_Stalinization,
+    Nuclear_Test_Ban,
+    Formosan_Resolution = 35,
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn check_cards() {
+        let cards = super::init_cards();
+        assert_eq!(cards.len(), 36);
+    }
 }
