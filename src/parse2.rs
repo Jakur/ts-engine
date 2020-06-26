447ddf8990c71fd1828853086e9f11556d086c38
Justin Kur <Justink97@hotmail.com>
2020-06-26 01:40:16 -0400

    Parse basic outcomes

--- src/parse2.rs
+++ src/parse2.rs
@@ -1,18 +1,22 @@
 use crate::card::{Card, Effect};
 use crate::country::Side;
 use crate::tensor::DecodedChoice;
-use pest::iterators::Pair;
+use pest::iterators::{Pair, Pairs};
 use pest::Parser;
 use std::collections::HashMap;
 
 lazy_static! {
     static ref CARD_NAMES: HashMap<String, Card> = {
-        let map = (1..Card::total())
+        let mut map: HashMap<_, _> = (1..Card::total())
             .map(|num| {
                 let card = Card::from_index(num);
                 (standard_card_name(card), card)
             })
             .collect();
+        let fix = [("John Paul II Elected Pope*", Card::John_Paul)];
+        for (k, v) in fix.iter() {
+            map.insert(k.to_string(), *v);
+        }
         map
     };
     static ref COUNTRY_INDEX: HashMap<String, usize> = { country_names() };
@@ -28,29 +32,229 @@ fn standard_card_name(card: Card) -> String {
     name
 }
 
+// fn get_card(name: &str) -> Option<Card> {
+//     CARD_NAMES.get(name.split_whitespace().next()?).copied()
+// }
+
+fn get_country(name: &str) -> Option<usize> {
+    COUNTRY_INDEX.get(name.split_whitespace().next()?).copied()
+}
+
+fn parse_num<T: std::str::FromStr>(num: Pair<Rule>) -> Option<T> {
+    num.as_str().parse::<T>().ok()
+}
+
+#[derive(Debug)]
+struct CountryChange {
+    index: usize,
+    us: i8,
+    ussr: i8,
+    delta: i8,
+}
+
+impl CountryChange {
+    fn new(index: usize, us: i8, ussr: i8, delta: i8) -> Self {
+        Self {
+            index,
+            us,
+            ussr,
+            delta,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct MilOps {
+    side: Side,
+    ops: i8,
+}
+
+impl MilOps {
+    fn new(side: Side, ops: i8) -> Self {
+        Self { side, ops }
+    }
+}
+
 #[derive(Debug)]
 // Outcome Order: US, USSR
 enum Outcome {
-    Country { index: usize, us: i8, ussr: i8 },
+    Country(CountryChange),
     Defcon(i8),
     Vp(i8),
     StartEffect(Effect), // Todo side of effect? (Harder than one would think)
     EndEffect(Effect),
-    MilOps(Side, i8),
-    War, // Todo
+    MilitaryOps(MilOps),
+    War {
+        target: usize,
+        roll: i8,
+        changes: Vec<CountryChange>,
+        mil_ops: MilOps,
+    },
     Space(Side, i8),
-    ConductOps, // Todo
 }
 
 fn parse_outcome(pair: Pair<Rule>) -> Option<Outcome> {
-    unimplemented!();
+    let rule = pair.as_rule();
+    let mut vals = pair.into_inner().into_iter();
+    match rule {
+        Rule::outcome => {
+            // Convert to its inner type and recurse
+            let inner = vals.next().expect("Must have an inner value");
+            parse_outcome(inner)
+        }
+        Rule::country_change => {
+            let delta = parse_num(vals.nth(1)?)?;
+            let name = vals.next().unwrap().as_str();
+            let index = get_country(name)?;
+            let (us, ussr) = parse_country_status(vals.next().unwrap())?;
+            Some(Outcome::Country(CountryChange {
+                index,
+                us,
+                ussr,
+                delta,
+            }))
+        }
+        Rule::vp_change => {
+            // Todo see if this actually works
+            let vp = if let Some(s) = vals.next() {
+                let side = parse_side(s)?;
+                let num = parse_num(vals.next()?)?;
+                match side {
+                    Side::US => num,
+                    Side::USSR => num * -1,
+                    Side::Neutral => unimplemented!(),
+                }
+            } else {
+                0
+            };
+            Some(Outcome::Vp(vp))
+        }
+        Rule::start_effect => {
+            let card = parse_card(vals.next()?)?;
+            let effect = Effect::card_to_effect(card)?;
+            Some(Outcome::StartEffect(effect))
+        }
+        Rule::end_effect => {
+            let card = parse_card(vals.next()?)?;
+            let effect = Effect::card_to_effect(card)?;
+            Some(Outcome::EndEffect(effect))
+        }
+        Rule::set_mil_ops => {
+            let side = parse_side(vals.next()?)?;
+            let num = parse_num(vals.next()?)?;
+            Some(Outcome::MilitaryOps(MilOps::new(side, num)))
+        }
+        Rule::war => {
+            let name = vals.next()?.as_str();
+            let target = get_country(name)?;
+            let roll = parse_num(vals.next()?)?;
+            let mut changes = Vec::new();
+            while let Some(pair) = vals.next() {
+                // Add country changes until we hit mil ops, i.e. the last line
+                let outcome = parse_outcome(pair)?;
+                match outcome {
+                    Outcome::Country(cc) => {
+                        changes.push(cc);
+                    }
+                    Outcome::MilitaryOps(mil_ops) => {
+                        return Some(Outcome::War {
+                            target,
+                            roll,
+                            changes,
+                            mil_ops,
+                        })
+                    }
+                    _ => return None,
+                }
+            }
+            None
+        }
+        Rule::space => {
+            let side = parse_side(vals.next()?)?;
+            let level = vals.next()?.as_str().parse().ok()?;
+            Some(Outcome::Space(side, level))
+        }
+        _ => None,
+    }
+}
+
+fn parse_country_status(pair: Pair<Rule>) -> Option<(i8, i8)> {
+    match pair.as_rule() {
+        Rule::country_status => {
+            let mut vals = pair.into_inner();
+            let us = vals.next().unwrap().as_str().parse().ok()?;
+            let ussr = vals.next().unwrap().as_str().parse().ok()?;
+            Some((us, ussr))
+        }
+        _ => None,
+    }
+}
+
+enum AR<'a> {
+    EventBefore {
+        event: Pair<'a, Rule>,
+        action: Pair<'a, Rule>,
+        card: Card,
+    },
+    EventAfter {
+        action: Pair<'a, Rule>,
+        event: Pair<'a, Rule>,
+        card: Card,
+    },
+    SimpleUse {
+        action: Pair<'a, Rule>,
+        card: Card,
+    },
+}
+
+impl<'a> AR<'a> {
+    fn consume(self, primary_side: Side) -> Vec<ActionRound> {
+        match self {
+            AR::EventBefore {
+                event,
+                action,
+                card,
+            } => {
+                let e_side = card.side();
+            }
+            _ => {}
+        }
+        vec![]
+    }
+}
+
+fn parse_action(action: Pair<Rule>) -> ActionRound {
+    use std::cell::RefCell;
+    let choices = RefCell::new(Vec::new());
+    let outcomes = RefCell::new(Vec::new());
+    inner(action, &choices, &outcomes);
+    fn inner(
+        x: Pair<Rule>,
+        choices: &RefCell<Vec<DecodedChoice>>,
+        outcomes: &RefCell<Vec<Outcome>>,
+    ) {
+        match x.as_rule() {
+            Rule::event => todo!(),
+            Rule::inf => {}
+            _ => todo!(),
+        }
+    }
+    let choices = choices.into_inner();
+    let outcomes = outcomes.into_inner();
+    ActionRound {
+        choices,
+        outcomes,
+        card: None,
+        actor: Side::Neutral,
+    }
 }
 
 #[derive(Debug)]
 pub struct ActionRound {
     choices: Vec<DecodedChoice>,
     outcomes: Vec<Outcome>,
-    card: Card, // Debug only
+    card: Option<Card>,
+    actor: Side,
 }
 
 fn parse_ar(pair: Pair<Rule>) -> Option<ActionRound> {
@@ -72,7 +276,8 @@ fn parse_ar(pair: Pair<Rule>) -> Option<ActionRound> {
             card.map(|c| ActionRound {
                 choices: vec![],
                 outcomes: vec![],
-                card: c,
+                card: Some(c),
+                actor: Side::Neutral,
             })
         }
         _ => None,
@@ -110,6 +315,7 @@ fn country_names() -> HashMap<String, usize> {
         .iter()
         .enumerate()
         .map(|(index, name)| {
+            // The key is the first word, i.e. until the next uppercase char
             let s = format!("{:?}", name);
             let end = s
                 .char_indices()
@@ -211,9 +417,12 @@ DEFCON degrades to 2
                 .expect("Bad parse")
                 .next()
                 .unwrap();
-            if count == 1 {
+            if count == 0 {
                 let ar = parse_ar(parsed);
-                assert_eq!(Some(Card::Portuguese_Empire_Crumbles), ar.map(|x| x.card));
+                assert_eq!(
+                    Some(Card::John_Paul),
+                    ar.map(|x| x.card.expect("Some card"))
+                );
             }
             // dbg!(parsed);
         }
